45298721eca308ec593148a9c3a675bff0dc7520
Krishna Prasad Varma <prasad@rightsolutions.io>
2017-09-29 15:50:59 +0400

    Dummy in profile serializer

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,6 +1,6 @@
 class ProfileSerializer < ActiveModel::Serializer
 	include NullAttributeReplacer
-  attributes :id, :name, :gender, :date_of_birth, :username, :email, :phone
+  attributes :id, :name, :gender, :date_of_birth, :username, :email, :phone, :dummy
 
   has_one :profile_picture, class_name: "Image::ProfilePicture", serializer: ProfilePictureSerializer
 end

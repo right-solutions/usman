5d7bdaee3d95b72254dc87d239150daaef999003
Krishna Prasad Varma <prasad@rightsolutions.io>
2017-12-12 15:47:37 +0400

    Added City and Country info in profile

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -9,4 +9,20 @@ class ProfileSerializer < ActiveModel::Serializer
       object.build_profile_picture
     end
   end
+
+  has_one :country, class_name: "Country", serializer: CountryPreviewSerializer do
+ 		if object.country
+      object.country
+    else
+      object.build_country
+    end
+ 	end
+ 		
+ 	has_one :city, class_name: "City", serializer: CityPreviewSerializer do
+ 		if object.city
+      object.city
+    else
+      object.build_city
+    end
+ 	end
 end
